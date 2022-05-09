096fc8ced5edd2858f964aa6e46ded4118a08332
Jaehun Kim <wogns3623@gmail.com>
2022-05-09 10:49:01 +0900

    FEAT: Add prettier plugin for tailwind

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,5 @@
 module.exports = {
-  plugins: [
-    require('@trivago/prettier-plugin-sort-imports'),
-  ],
+  plugins: [require('@trivago/prettier-plugin-sort-imports'), require('prettier-plugin-tailwindcss')],
   arrowParens: 'avoid',
   jsxSingleQuote: true,
   bracketSameLine: true,
